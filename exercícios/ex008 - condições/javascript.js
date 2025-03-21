6de44a29bbedf281e7d203740f9fe0c2b50470b7
Guilherme Becker <gui.becker@live.com>
2025-03-21 15:27:16 -0300

    aaaaaaaa javascript é mt chato

---
+++ exercícios/ex008 - condições/javascript.js
@@ -0,0 +1,5 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {     // condição simples
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
---
+++ exercícios/ex009 - condições/javascript.js
@@ -0,0 +1,7 @@
+var país = "Brasil"
+console.log(`vivendo em ${país} você é:`)
+if (país == 'Brasil') {     // condição composta
+    console.log('você é brasiliano')
+} else {
+    console.log('você é estranho')
+}
