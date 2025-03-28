e2bcf7681cfda12ffae3eab2e6561356624bf8ec
Guilherme Becker <gui.becker@live.com>
2025-03-28 17:30:42 -0300

    loops

---
+++ exercícios/aula13/ex/testeLogicoFinal.js
@@ -0,0 +1,10 @@
+let litros = 80;
+let km = 0;
+
+do {
+    litros--;
+    km += 10;
+} while (litros > 0);
+
+console.log(`você rodou ${km} quilometros`)
+console.log(`${litros}`)
---
+++ exercícios/aula13/ex/testeLogicoInicio.js
@@ -0,0 +1,8 @@
+let litros = 80;
+let km = 0;
+
+while (litros > 0) {
+    litros--
+    km += 10
+}
+console.log(`você rodou ${km} quilometros`)
