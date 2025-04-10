c036dbfd5a191cd3fbc723bfd8fe0a1af5714903
Guilherme Becker <gui.becker@live.com>
2025-04-10 14:52:20 -0300

    att

---
+++ exercícios - CursoemVideo/projetos/calculadora-de-tempo/script.js
@@ -0,0 +1,13 @@
+const segundo = 1;
+const minuto = 60 * segundo;
+const hora = 60 * minuto;
+const dia = 24 * hora;
+const semana = 7 * dia
+const mes = 4 * semana;
+const ano = 365 * dia;
+
+const campo1 = document.querySelector('#field1')
+const campo2 = document.querySelector('#field2')
+const tempo1 = document.querySelector('#tempo1')
+const tempo2 = document.querySelector('#tempo2')
+
